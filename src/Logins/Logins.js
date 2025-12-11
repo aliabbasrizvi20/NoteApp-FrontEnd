00618449b44de5e5c91830ed2318de56a5225416
aliabbasrizvi20 <rockcodes0@gmail.com>
2025-12-11 20:02:42 +0530

    latest commits

--- src/Logins/Logins.js
+++ src/Logins/Logins.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 import "./Logins.css";
 import axiosClient from "../api/axiosClient";
 export default function Login() {
